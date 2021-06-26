85a92fd9f5287a1c220e218100511354ae42097d
CUBE-1997\Dani <daniel.lenart@kosickaakademia.sk>
2021-06-26 15:16:14 +0200

    added missing migrations

---
+++ BasicSealBackend/Migrations/20201228133354_firstMigration.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace BasicSealBackend.Migrations
+{
+    public partial class firstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Softwares",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<long>(nullable: false),
+                    SoftwareName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Softwares", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Softwares_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SoftwareLicenses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    SoftwareId = table.Column<long>(nullable: false),
+                    LicenseKey = table.Column<string>(nullable: true),
+                    LicenseLength = table.Column<int>(nullable: false),
+                    LicenseStartDate = table.Column<DateTime>(nullable: false),
+                    LicenseEndDate = table.Column<DateTime>(nullable: false),
+                    IsLifetime = table.Column<bool>(nullable: false),
+                    IsActivated = table.Column<bool>(nullable: false),
+                    IsBanned = table.Column<bool>(nullable: false),
+                    enableOfflineVerification = table.Column<bool>(nullable: false),
+                    CpuIdentifier = table.Column<string>(nullable: true),
+                    HddIdentifier = table.Column<string>(nullable: true),
+                    SystemUUID = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SoftwareLicenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SoftwareLicenses_Softwares_SoftwareId",
+                        column: x => x.SoftwareId,
+                        principalTable: "Softwares",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SoftwareVersions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<long>(nullable: false),
+                    SoftwareId = table.Column<long>(nullable: false),
+                    Version = table.Column<string>(nullable: true),
+                    Hash = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SoftwareVersions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SoftwareVersions_Softwares_SoftwareId",
+                        column: x => x.SoftwareId,
+                        principalTable: "Softwares",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SoftwareVersions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SoftwareLicenses_SoftwareId",
+                table: "SoftwareLicenses",
+                column: "SoftwareId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Softwares_UserId",
+                table: "Softwares",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SoftwareVersions_SoftwareId",
+                table: "SoftwareVersions",
+                column: "SoftwareId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SoftwareVersions_UserId",
+                table: "SoftwareVersions",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SoftwareLicenses");
+
+            migrationBuilder.DropTable(
+                name: "SoftwareVersions");
+
+            migrationBuilder.DropTable(
+                name: "Softwares");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ BasicSealBackend/Migrations/20210123155723_addedDeleteProperty.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BasicSealBackend.Migrations
+{
+    public partial class addedDeleteProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "SoftwareVersions",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "Softwares",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "SoftwareLicenses",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "SoftwareVersions");
+
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "Softwares");
+
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "SoftwareLicenses");
+        }
+    }
+}
